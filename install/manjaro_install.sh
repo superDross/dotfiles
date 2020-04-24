c173d446d45e5c80af597d41c3e0e1cabd80b8b9
David Ross <dross78375@gmail.com>
2020-04-24 12:36:05 +0100

    Added .psql notes

--- install/manjaro_install.sh
+++ install/manjaro_install.sh
@@ -154,6 +154,7 @@ lmxappearance
   ln -s ${DOTFILESDIR}/vim/vimrc ~/.vimrc
   ln -s ${DOTFILESDIR}/bash/bashrc ~/.bashrc
   ln -s ${DOTFILESDIR}/bash/inputrc ~/.inputrc
+  ln -s ${DOTFILESDIR}/postgres/psqlrc ~/.psqlrc
   cp ${DOTFILESDIR}/terminal/terminalrc  ~/.config/xfce4/terminal/
   ln -s ${DOTFILESDIR}/tmux/tmux.conf ~/.tmux.conf
   ln -s ${DOTFILESDIR}/i3/config ~/.i3/config
