d883b07907fd01e5250bb71a7db67c75cd380e54
David Ross <dross78375@gmail.com>
2020-05-26 20:39:31 +0100

    Increase nvim compatability

--- notes/algorithms/interpolation_search.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Optional
-
-
-def interpolation_search(items: List[int], item: int) -> Optional[int]:
-    print(item-items[0])
-
-
-interpolation_search([4, 5, 8, 10, 45], 10)
