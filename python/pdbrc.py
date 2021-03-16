49c10fe4ab0edfeb2fe266ea0961c3c70bb2b39e
David Ross <dross78375@gmail.com>
2021-03-16 12:49:09 +0000

    update with alias

--- python/pdbrc.py
+++ python/pdbrc.py
@@ -6,3 +6,10 @@ class Config(pdb.DefaultConfig):
     editor = "vim"
     truncate_long_lines = False
 
+    def _set_aliases(self, pdb):
+        # make 'l' an alias to 'longlist'
+        Pdb = pdb.__class__
+        Pdb.do_l = Pdb.do_longlist
+
+    def setup(self, pdb):
+        self._set_aliases(pdb)
