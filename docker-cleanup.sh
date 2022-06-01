c46f37978f3ac8e9e33faa110abe13df30506552
David Ross <dross78375@gmail.com>
2022-06-01 15:20:38 +0100

    cleanup script

---
+++ docker-cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker system prune
+docker container stop $(docker container ls -aq)
+docker container rm $(docker container ls -aq)
+docker rmi $(docker images -aq)
+docker volume prune
