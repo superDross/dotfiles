baf9cceb69b85938a572d3e3f4744ba91820174e
David Ross <dross78375@gmail.com>
2022-07-03 21:22:43 +0100

    Add new command to remove all docker related files

--- docker-cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker system prune
-docker container stop $(docker container ls -aq)
-docker container rm $(docker container ls -aq)
-docker rmi $(docker images -aq)
-docker volume prune
