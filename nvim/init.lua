f46083d247503a1796cf0f935f83e220d6818e11
David Ross <dross78375@gmail.com>
2022-09-05 19:49:41 +0100

    Use fzf by default with ticket.vim in vim & nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -315,6 +315,7 @@ vim.g.noteurl = 'https://github.com/superDross/dotfiles/blob/master/notes/'
 vim.g.auto_ticket_open = 1
 vim.g.auto_ticket_git_only = 1
 vim.g.ticket_black_list = {'main', 'master'}
+vim.g.ticket_use_fzf_default = 1
 
 
 -- STATUSLINE ------------------------------------------------------------
