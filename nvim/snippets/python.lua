4c7775e35f57bf37c14d130d99290aa1c38dbde2
David Ross <dross78375@gmail.com>
2022-09-22 18:31:30 +0100

    Add class constructor snippet

--- nvim/snippets/python.lua
+++ nvim/snippets/python.lua
@@ -10,7 +10,6 @@ local dynamic = ls.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require('luasnip.extras').rep
 
--- Debugger
 
 local pdb = snip({
   trig = "pdb",
@@ -28,23 +27,40 @@ local rpdb = snip({
   text("from remote_pdb import RemotePdb;RemotePdb('0.0.0.0', 4444).set_trace()  # fmt: skip")
 })
 
-local example = snip({
-  trig = "example",
-  name = "example",
-  descr = "an example with insert nodes",
-}, fmt([[
-  class {}: 
-      {}  # test
-      {}
-  ]], {
-  -- i(1) is at nodes[1], i(2) at nodes[2].
-  insert(1, "PlaceHolder"), insert(2, "pass"), insert(3, "33"),
-}))
-
-
-
--- TODO: create a class builder
+-- TODO: use fmt instead of text nodes here for greater readability
+--
+local class_constructor = snip({
+  trig = 'cls',
+  name = 'class',
+  dscr = 'Class Constructor',
+}, {
+  text('class '),
+  insert(1, { 'MyClass' }),
+  -- text('('),
+  -- insert(2, { '' }),
+  text({ ':', '\t' }),
+  text({ 'def init(self,' }),
+  insert(2),
+  text({ '):', '' }),
+  func(function(args)
+    if not args[1][1] or args[1][1] == '' then
+      return { '' }
+    end
+    local a = vim.tbl_map(function(item)
+      local trimed = vim.trim(item)
+      return '\t\tself.' .. trimed .. ' = ' .. trimed
+    end, vim.split(
+      args[1][1],
+      ',',
+      true
+    ))
+    return a
+  end, {
+    2,
+  }),
+  insert(0),
+})
 
 ls.add_snippets(nil, {
-  python = { pdb, rpdb, example }
+  python = { pdb, rpdb, class_constructor }
 })
