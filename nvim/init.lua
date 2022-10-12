7698ff445f735089688966e925ba4a8c8de9abd0
David Ross <dross78375@gmail.com>
2022-10-13 00:26:31 +0100

    Use shell formatters and linters in nvim
    - shfmt and shellharden for formatting
    - shellcheck for linting

--- nvim/init.lua
+++ nvim/init.lua
@@ -362,7 +362,7 @@ mason_lspconfig.setup {
 mason_installer.setup {
   ensure_installed = {
     'black', 'flake8', 'isort', 'hadolint', 'jq', 'prettier', 'shfmt',
-    'vint', 'sql-formatter', 'stylua', 'luacheck',
+    'vint', 'sql-formatter', 'stylua', 'luacheck', 'shellharden', 'shellcheck'
   },
 }
 
@@ -376,10 +376,11 @@ end
 -- setup formatters and linters
 local d, f = null_ls.builtins.diagnostics, null_ls.builtins.formatting
 local flake8_config = { diagnostics_postprocess = swap_error_warning }
+local shfmt_config = { extra_args = {'-i', '4'} }  -- use 4 spaces
 null_ls.setup({
   sources = {
     d.hadolint, d.vint, d.flake8.with(flake8_config),
-    f.black, f.isort, f.jq, f.shfmt, f.sql_formatter,
+    f.black, f.isort, f.jq, f.shfmt.with(shfmt_config), f.sql_formatter, f.shellharden,
   }
 })
 
