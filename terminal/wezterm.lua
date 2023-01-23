b6c4b41368de843d0c8d87239b0a2c853d1b6ab8
David Ross <dross78375@gmail.com>
2023-01-23 07:06:31 +0000

    add wezterm config

---
+++ terminal/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font('Roboto Mono', { weight='Bold'}),
+  colors = {
+    foreground='#eeeeeeeeecec',
+    background='#1d1d20202121',
+  },
+  window_frame = {
+    font = wezterm.font { family = 'Roboto Mono', weight = 'Bold' },
+  },
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  hide_tab_bar_if_only_one_tab = true,
+  tab_bar_at_bottom = true,
+}
