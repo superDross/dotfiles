5c3f2a21b45041584669ced039846385ad737a5f
David Ross <dross78375@gmail.com>
2024-03-21 19:56:08 +0000

    Force 4 space tab by default

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,6 +22,8 @@ vim.o.thesaurus = '~/.vim/thesaurus.txt'            -- Ctrl-x,Ctrl-t
 vim.o.mouse = nil
 vim.o.sessionoptions = 'buffers,curdir,help,tabpages,terminal,winsize'
 vim.o.shortmess = vim.o.shortmess .. 'c'
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.opt.fillchars = { eob = " " }  -- prevent ~ at end of file
 vim.g.vimrc = vim.fn.resolve(vim.fn.expand('<sfile>:p'))
 vim.g.vimdir = vim.fn.fnamemodify(vim.g.vimrc, ':h')
