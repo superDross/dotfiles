15ec5a4b3dd430a6084da75b0c897d80681fc0fc
David Ross <dross78375@gmail.com>
2025-10-08 16:12:34 +0100

    change docker setup

--- nvim/nvim.bash
+++ nvim/nvim.bash
@@ -1,3 +1,4 @@
 export GOPATH=$HOME/go
 export PATH=$PATH:$HOME/.local/share/bob/nvim-bin:$HOME/.cargo/bin:$GOPATH:/bin
 export TERM=xterm-256color
+# TODO: colours are off in tmux which makes sense bash -> tmux -> docker bash -> neovim
